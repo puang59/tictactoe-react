345e0d859119ff3f80cf9c421ac50a63cabdaa00
rezuwii <59052194+rezuwii@users.noreply.github.com>
2022-05-01 21:09:26 +0530

    fixed handleClickEvent

--- src/index.js
+++ src/index.js
@@ -12,15 +12,13 @@ const Square = (props) => {
 }
 
 const Board = () => {
-  const initialSquares = [
-    null, null, null,
-    null, null, null,
-    null, null, null,
-  ]
+  const initialSquares = Array(9).fill(null)
   const [squares, setSquare] = useState(initialSquares)
 
   const handleClickEvent = (i) => {
-    alert(`square ${i} clicked!`)
+    const newSquares = [...squares]
+    newSquares[i] = 'X'
+    setSquare(newSquares)
   }
 
   const renderSquare = (i) => {
