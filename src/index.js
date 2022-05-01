d8897cb21bec9cac58806edecbf47cdc1e2dbecf
rezuwii <59052194+rezuwii@users.noreply.github.com>
2022-05-01 21:29:15 +0530

    winner calculator added

--- src/index.js
+++ src/index.js
@@ -32,7 +32,8 @@ const Board = () => {
     )
   }
 
-  const status = `Next Player: ${xIsNext ? 'X' : 'O'}`
+  const winner = calculateWinner(squares)
+  const status = winner ? `Winner: ${winner}` : `Next Player: ${xIsNext ? 'X' : 'O'}`
 
   return (
     <div className='status'>
@@ -63,3 +64,20 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 )
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], //rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], //columns
+    [0, 4, 8], [2, 4, 6] //diagonal
+  ]
+
+  for (let line of lines) {
+    const [a, b, c] = line
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
